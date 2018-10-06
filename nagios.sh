748717682bc829eab54531ed59ec18581296a935
root <aschults@gmx.net>
2018-10-06 09:26:51 +0200

    Various

--- nagios.sh
+++ nagios.sh
@@ -3,8 +3,8 @@ set -e
 mkdir -p /var/lib/nagios/spool/checkresults
 mkdir -p /var/lib/nagios/rw
 mkdir -p /var/cache/nagios
+mkfifo -m 666 /tmp/lighttpd_logs
 sed -i -e "s!^date.timezone.*!date.timezone=$TZ!" /etc/php5/php.ini
 echo "$TZ" >/etc/TZ
 ln -sf /usr/share/zoneinfo/$TZ /etc/localtime
-/usr/sbin/lighttpd -f /etc/lighttpd/lighttpd.conf -D &
-/usr/sbin/nagios /etc/nagios/nagios.cfg
+supervisord
